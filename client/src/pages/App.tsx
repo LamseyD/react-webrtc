3ac4693e0cd4be6fe64c7341d43de15c853cc416
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2021-05-15 22:26:10 -0400

    feat: updated landing page

---
+++ client/src/pages/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ChakraProvider, Flex } from "@chakra-ui/react"
+import { Navbar } from '../components/Navbar';
+import { VideoPlayer } from '../components/VideoPlayer';
+import { Options } from '../components/Options';
+import { Notifications } from '../components/Notifications';
+
+
+const App: React.FC = () => {
+  return (
+    <ChakraProvider>
+      <Flex bg="#1c1c1e" flexDirection="column" height="100vh">
+        <Navbar />
+        <Flex>
+          <VideoPlayer />
+          <Options>
+            <Notifications />
+          </Options>
+        </Flex>
+      </Flex>
+    </ChakraProvider>
+  );
+}
+
+export default App;
